a87e08dfc73272e3eaf51a5385c746d425b17b29
GeckoProjects <darmantra@gmail.com>
2017-12-02 21:46:36 +0800

    Added edit function

--- assets/js/input.js
+++ assets/js/input.js
@@ -23,12 +23,31 @@
 			$(this).closest( '#' + $(this).data('parent') ).find('ul').append( custom_choice_template( $(this).data('parent'), $(this).data('name') ) );
 		});
 
-		$('body').on('click', '.btn-custom-choice-save', function(e) {
+		$('body').on('click', '.btn-custom-choice-save-edit', function(e) {
 			e.preventDefault();
 			var value = $(this).closest('li').find('input[type="text"]').val();
 			var id = $(this).closest('li').find('input[type="checkbox"]').attr('id');
-			$(this).closest('li').find('input[type="checkbox"]').attr('id', id + '_' + value.toLowerCase());
-			$(this).closest('li').find('input[type="checkbox"]').attr('value', value.toLowerCase());
+
+			if( value.trim() == '' ) {
+				$(this).closest('li').find('input[type="text"]').addClass('custom-checkbox-has-error');
+				return;
+			}
+
+			$(this).closest('li').find('input[type="checkbox"]').attr('id', '');
+			$(this).closest('li').find('input[type="checkbox"]').attr('id', id + '_' + value.replace(/\s+/g, '-').toLowerCase());
+			$(this).closest('li').find('input[type="checkbox"]').attr('value', value);
+			$(this).closest('li').find('.custom-checkbox-value').text(value);
+			$(this).closest('li').find('input[type="text"]').removeClass('custom-checkbox-has-error');
+
+			if( $(this).find('span').hasClass('dashicons-yes') ) {
+				$(this).closest('li').find('input[type="text"]').hide();
+				$(this).closest('li').find('.custom-checkbox-value').show();
+				$(this).html('<span class="dashicons dashicons-edit"></span>');
+			}else{
+				$(this).closest('li').find('input[type="text"]').show();
+				$(this).closest('li').find('.custom-checkbox-value').hide();
+				$(this).html('<span class="dashicons dashicons-yes"></span>');
+			}
 		});
 
 		$('body').on('click', '.btn-custom-choice-remove', function(e) {
@@ -38,7 +57,13 @@
 	}
 
 	function custom_choice_template(id, name) {
-		var template = '<li><label><input type="checkbox" id="'+ id +'" name="' + name + '" class="custom_checkbox" checked="checked"><input type="text"><button class="btn-custom-choice-save">Save</button><button class="btn-custom-choice-remove">Remove</button></label></li>';
+		var template = '<li><label><input type="checkbox" id="'+ id +'" name="' + name + '" class="custom_checkbox" checked="checked">'+
+		'<input type="text"><span class="custom-checkbox-value" style="display:none"></span>'+
+		'<div class="custom-button-group">' +
+		'<a href="#" class="btn-custom-choice-save-edit"><span class="dashicons dashicons-yes"></span></a>'+
+		'<a href="#" class="btn-custom-choice-remove"><span class="dashicons dashicons-minus"></span></a>'+
+		'</div>' +
+		'</label></li>';
 
 		return template;
 	}
